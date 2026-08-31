d5c4fce2e162d1a478b23af6441080c2683fdb02
Nishant Thapa <nishantjargha24@gmail.com>
2026-08-30 19:47:03 -0500

    Create TicketProgram.java

---
+++ TicketProgram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class TicketProgram {
+  public static void main (String[] args) {
+    final int TICKET_COST = 15;
+    Scanner keyboard = new Scanner(System.in);
+    int tickets;
+    double total;
+
+    System.out.print("Enter the number of tickets: ");
+    tickets = keyboard.nextInt();
+    total = tickets * TICKET_COST;
+    System.out.println("Total is $ " + total);
+    keybaord.close();
+  }
+}
