8f2d3d836bad4321200487c37afb4ad3cbb03354
Nishant Thapa <nishantjargha24@gmail.com>
2026-08-30 22:52:04 -0500

    Update TicketProgram.java

--- TicketProgram.java
+++ TicketProgram.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class TicketProgram {
   public static void main (String[] args) {
     final int TICKET_COST = 15;
-    Scanner keyboard = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
     int tickets;
     double total;
 
     System.out.print("Enter the number of tickets: ");
-    tickets = keyboard.nextInt();
+    tickets = scanner.nextInt();
     total = tickets * TICKET_COST;
     System.out.println("Total is $ " + total);
-    keyboard.close();
+    scanner.close();
   }
 }
