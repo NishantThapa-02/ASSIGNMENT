c6817deecf1c5a84119f7511207aa2574f279688
Nishant Thapa <nishantjargha24@gmail.com>
2026-09-02 23:25:35 -0500

    Change variable types to double for area calculations
    Updated variable types for side, radius, base, and height to double for more accurate calculations. Adjusted input methods to use nextDouble for square, circle, and triangle calculations.

--- AreaProgram.java
+++ AreaProgram.java
@@ -13,28 +13,29 @@ public class AreaProgram {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
 	int option;
-	int side, radii, baseY, heightY;
+	double side, radius, base, height;
     do {
 	  System.out.print("=== AREA CALCULATOR ===\n1. Square\n2. Circle\n3. Triangle\n4. Exit\n");
 	  System.out.print("Enter the desired option[1-4]: ");
 	  option = keyboard.nextInt();
 	  if (option == 1) {
         System.out.print("Enter the length of the square: ");
-		side = keyboard.nextInt();
+		side = keyboard.nextDouble();
 		// print out the area of the square
 		System.out.println("The area of the square is: " + (side * side));
 	  } else if (option == 2) {
         System.out.print("Enter the radius of the circle: ");
-		radii = keyboard.nextInt();
+		radius = keyboard.nextInt();
 		// print out the area of the circle
-		System.out.println("The area of the circle is: " + (Math.PI * radii * radii));
+		System.out.println("The area of the circle is: " + (Math.PI * radius * radius));
 	  } else if (option == 3) {
         System.out.print("Enter the base of the triangle: ");
-		baseY = keyboard.nextInt();
+		base = keyboard.nextDouble();
 		System.out.print("Enter the height of the triangle: ");
-		heightY = keyboard.nextInt();
+		height = keyboard.nextDouble();
+		double area = (0.5) * base * height;
 		// print out the area of the triangle
-		System.out.println("The area of the triangle is: " + ((0.5) * baseY * heightY));
+		System.out.println("The area of the triangle is: " + area);
 	  }else if (option == 4) {
         System.out.println("Exiting the program.");
 	  } else {
