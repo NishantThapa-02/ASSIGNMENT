dcd22a964bc7125f213fd01d7283757466e66812
Nishant Thapa <nishantjargha24@gmail.com>
2026-09-02 23:10:25 -0500

    Update AreaProgram.java

--- AreaProgram.java
+++ AreaProgram.java
@@ -1,38 +1,46 @@
 import java.util.Scanner;
 import java.lang.Math;
 
+/**
+ * AreaProgram is an application that can calculate the area.
+ */
 public class AreaProgram {
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-		int option;
-		int side, radii, baseY, heightY;
-
-		do {
-			System.out.print("=== AREA CALCULATOR ===\n1. Square\n2. Circle\n3. Triangle\n4. Exit\n");
-			System.out.print("Enter the desired option[1-4]: ");
-			option = keyboard.nextInt();
-
-			if (option == 1) {
-        			System.out.print("Enter the length of the square: ");
-				side = keyboard.nextInt();
-				System.out.println("The area of the square is: " + side * side);
-			} else if(option == 2) {
-        			System.out.print("Enter the radius of the circle: ");
-				radii = keyboard.nextInt();
-				System.out.println("The area of the circle is: " + Math.PI * radii * radii);
-			} else if (option == 3) {
-        			System.out.print("Enter the base of the triangle: ");
-				baseY = keyboard.nextInt();
-				System.out.print("Enter the height of the triangle: ");
-				heightY = keyboard.nextInt();
-				System.out.println("The area of the triangle is: " + (0.5) * baseY * heightY);
-			}else if (option == 4) {
-        			System.out.println("Exiting the program.");
-			} else {
-				System.out.println("Invalid option!! You must choose from 1-4.");
-			}
-		} while (option != 4);
-
-		keyboard.close();
-	}
+  /**
+  * It executes the area application
+  * 
+  * @param args
+  */
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+	int option;
+	int side, radii, baseY, heightY;
+    do {
+	  System.out.print("=== AREA CALCULATOR ===\n1. Square\n2. Circle\n3. Triangle\n4. Exit\n");
+	  System.out.print("Enter the desired option[1-4]: ");
+	  option = keyboard.nextInt();
+	  if (option == 1) {
+        System.out.print("Enter the length of the square: ");
+		side = keyboard.nextInt();
+		// print out the area of the square
+		System.out.println("The area of the square is: " + (side * side));
+	  } else if (option == 2) {
+        System.out.print("Enter the radius of the circle: ");
+		radii = keyboard.nextInt();
+		// print out the area of the circle
+		System.out.println("The area of the circle is: " + (Math.PI * radii * radii));
+	  } else if (option == 3) {
+        System.out.print("Enter the base of the triangle: ");
+		baseY = keyboard.nextInt();
+		System.out.print("Enter the height of the triangle: ");
+		heightY = keyboard.nextInt();
+		// print out the area of the triangle
+		System.out.println("The area of the triangle is: " + ((0.5) * baseY * heightY));
+	  }else if (option == 4) {
+        System.out.println("Exiting the program.");
+	  } else {
+		System.out.println("Invalid option!! You must choose from 1-4.");
+	  }
+    } while (option != 4);
+    keyboard.close();
+  }
 }
