b8b092ae6c0ae6a974616b3a0eea9c97bbc0e149
Nishant Thapa <nishantjargha24@gmail.com>
2026-09-13 19:14:53 -0500

    Enhance Student.java with comments for clarity
    Added comments to clarify private variables, constructors, and object creation.

--- Student.java
+++ Student.java
@@ -1,15 +1,16 @@
-import java.lang.*;
-
 public class Student {
+    //private variables, no one can access this information outside of the class
     private String firstName;
     private String lastName;
     private String major;
     private double gpa;
     
+    // set the value of the variables
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-    
+
+    //get the value of the variables from user or object 
     public String getFirstName() {
         return firstName;
     }
@@ -38,10 +39,12 @@ public class Student {
         return gpa;
     }
     
+    // constructor method, initialize the value of the object
     public Student() {
         this("", "", "", 0.0);
     }
     
+    // constructor with parameters, initialize the value of the object after getting the input from user
     public Student(String firstName, String lastName, String major, double gpa) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -50,15 +53,23 @@ public class Student {
     }
     
     public static void main(String[] args) {
+        // creating new object name student1
         Student student1 = new Student();
         System.out.println("First name: " + student1.getFirstName());
         System.out.println("Last name: " + student1.getLastName());
         System.out.println("Major: " + student1.getMajor());
         System.out.println("GPA: " + student1.getGpa());
+        // creating new object name student2 and initializing its value
         Student student2 = new Student("Peter", "Parker", "CS", 3.5);
         System.out.println("First name: " + student2.getFirstName());
         System.out.println("Last name: " + student2.getLastName());
         System.out.println("Major: " + student2.getMajor());
         System.out.println("GPA: " + student2.getGpa());
+        // creating new object name student3 and initializing its value
+        Student student3 = new Student("Harry", "Potter", "CS", 3.9);
+        System.out.println("First name: " + student3.getFirstName());
+        System.out.println("Last name: " + student3.getLastName());
+        System.out.println("Major: " + student3.getMajor());
+        System.out.println("GPA: " + student3.getGpa());
     }
 }
